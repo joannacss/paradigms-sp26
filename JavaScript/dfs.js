f92c8d9ac70815592888b07a5c41dd5444817a49
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-02-04 13:45:06 -0500

    Added toZString method

--- JavaScript/dfs.js
+++ JavaScript/dfs.js
@@ -85,7 +85,24 @@ class Graph {
         }
         return result;
     }
+
+    toString() {
+        let result = ["digraph G{ "];
+        this.adjacencyList.forEach((v, k) => {
+            
+            for(let i = 0 ; i < v.length ; i++){
+                result.push(`\t"${k}" -> "${v[i]}";`)
+            }
+            
+        });
+        result.push("}");
+        
+        return result.join("\n");
+    }
+  
 }
+
+
 
 // Examples:
 let g1 = new Graph();
@@ -93,21 +110,21 @@ g1.addEdge("+", "*");
 g1.addEdge("+", 3);
 g1.addEdge("*", 2);
 g1.addEdge("*", 7);
-console.log(g1);
-console.log(g1.dfs("+"));
-console.log(g1.bfs("+"));
+console.log(g1 + "");
+// console.log(g1.dfs("+"));
+// console.log(g1.bfs("+"));
 
 
-let g2 = new Graph();
-g2.addEdge(0, 1);
-g2.addEdge(0, 3);
-g2.addEdge(1, 2);
-g2.addEdge(1, 3);
-g2.addEdge(2, 3);
-g2.addEdge(2, 1);
-g2.addEdge(3, 0);
-g2.addEdge(3, 1);
-console.log(g2);
-console.log(g2.dfs(0));
-console.log(g2.bfs(0));
+// let g2 = new Graph();
+// g2.addEdge(0, 1);
+// g2.addEdge(0, 3);
+// g2.addEdge(1, 2);
+// g2.addEdge(1, 3);
+// g2.addEdge(2, 3);
+// g2.addEdge(2, 1);
+// g2.addEdge(3, 0);
+// g2.addEdge(3, 1);
+// console.log(g2);
+// console.log(g2.dfs(0));
+// console.log(g2.bfs(0));
 
--- JavaScript/live_dfs.js
+++ JavaScript/live_dfs.js
@@ -4,7 +4,7 @@ class Graph {
   // constructor declaration (init adjacencyList attribute)
   
 
-  // method to add a Node `n` to the graph
+  // method to add a Node `n` to the graph (if it does not exist yet)
   
   
   // method to add edge n1 -> n2
@@ -13,43 +13,50 @@ class Graph {
   // method to traverse the graph in DFS fashion
   
     // Initializes relevant data structures
-        // a stack initialized with the startNode
-        // a set that tracks nodes already visited
-        // the output to be returned
+      // a stack initialized with the startNode
+      // a set that tracks nodes already visited
+      // the output to be returned
 
     
     // iterate over each node to be visited (in the stack)
-    
+
       // pop node from the stack
-    
+
       // check if node is visited
-    
+
         // add node to the output
-    
+
         // mark node as visited
-    
-        // traverse children nodes
-    
+
+        // traverse children nodes (left-to-right--reversed)
+
     // return the results (list of nodes)
-    
 
   // method to traverse the graph in a BFS fashion
+  
 
+  // toString method
+  toString(){
+    return "graph";
+  }
+  
+  
 
 }
 
 // Test cases:
-// let g1 = new Graph();
+let g1 = new Graph();
 // g1.addEdge("+", "*");
 // g1.addEdge("+", 3);
 // g1.addEdge("*", 2);
 // g1.addEdge("*", 7);
-// console.log(g1);
+
 // console.log(g1.dfs("+"));
-// console.log(g1.bfs("+"));
+console.log(g1 + "")
 
 
-// let g2 = new Graph();
+
+let g2 = new Graph();
 // g2.addEdge(0, 1);
 // g2.addEdge(0, 3);
 // g2.addEdge(1, 2);
@@ -58,7 +65,7 @@ class Graph {
 // g2.addEdge(2, 1);
 // g2.addEdge(3, 0);
 // g2.addEdge(3, 1);
-// console.log(g2);
+
 // console.log(g2.dfs(0));
-// console.log(g2.bfs(0));
+
 
