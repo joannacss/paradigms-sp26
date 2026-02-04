01d143f98c8b46dc3118c809658204ebcf296046
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-02-04 10:59:21 -0500

    Updated live demo code

--- JavaScript/live_dfs.js
+++ JavaScript/live_dfs.js
@@ -1,69 +1,51 @@
+// Object type declaration (using class keyword)
 class Graph {
-  constructor() {
-    this.adjacencyList = new Map();
-  }
 
-  addNode(n) {
-    if (!this.adjacencyList.has(n)) {
-      this.adjacencyList.set(n, []);
-    }
-  }
-  // n1 -> n2
-  addEdge(n1, n2) {
-    if (!this.adjacencyList.has(n1)) {
-      this.addNode(n1);
-    }
-    if (!this.adjacencyList.has(n2)) {
-      this.addNode(n2);
-    }
+  // constructor declaration (init adjacencyList attribute)
+  
 
-    this.adjacencyList.get(n1).push(n2);    
-  }
+  // method to add a Node `n` to the graph
+  
+  
+  // method to add edge n1 -> n2
+  
 
-  dfs(startNode) {
+  // method to traverse the graph in DFS fashion
+  
     // Initializes relevant data structures
-    let stack = []  // a stack initialized with the startNode
-    let visited = new Set()  // a set that tracks nodes already visited
-    let results = []  // the output to be returned
+        // a stack initialized with the startNode
+        // a set that tracks nodes already visited
+        // the output to be returned
 
-    stack.push(startNode);
+    
     // iterate over each node to be visited (in the stack)
-    while(stack.length != 0){
-
+    
       // pop node from the stack
-      let current = stack.pop();
+    
       // check if node is visited
-      if(!visited.has(current)){
+    
         // add node to the output
-        results.push(current);
+    
         // mark node as visited
-        visited.add(current);
+    
         // traverse children nodes
-        let children = this.adjacencyList.get(current);
-        for(let i = children.length - 1; i >= 0 ; i--){
-          stack.push(children[i]);
-        }
-      }
-    }
+    
     // return the results (list of nodes)
-    return results;
+    
 
-  }
+  // method to traverse the graph in a BFS fashion
 
-  bfs(startNode) {
-    // TODO: (as an activity!)
-  }
 
 }
 
 // Test cases:
-let g1 = new Graph();
-g1.addEdge("+", "*");
-g1.addEdge("+", 3);
-g1.addEdge("*", 2);
-g1.addEdge("*", 7);
+// let g1 = new Graph();
+// g1.addEdge("+", "*");
+// g1.addEdge("+", 3);
+// g1.addEdge("*", 2);
+// g1.addEdge("*", 7);
 // console.log(g1);
-console.log(g1.dfs("+"));
+// console.log(g1.dfs("+"));
 // console.log(g1.bfs("+"));
 
 
