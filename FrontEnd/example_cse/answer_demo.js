3ca8d992d827c1d9fa8a234d894ed6abcbf0094f
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-02-11 13:23:42 -0500

    Added HW3

---
+++ FrontEnd/example_cse/answer_demo.js
@@ -0,0 +1,8 @@
+// Q1: Use DOM to change the H1 element to “Computer Science and Engineering Faculty”
+
+
+// Q2: Change everyone’s names to be Prof. Last Name
+
+
+// Q3: Change Dr. Cleland-Huang’s link to her lab website (https://sarec.nd.edu)
+
