665a675323be3f50c44c926c42e62685fae36cda
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-04-27 14:34:42 -0400

    live demo

--- Clojure/LiveCityTemperatures.clj
+++ Clojure/LiveCityTemperatures.clj
@@ -1,59 +1,30 @@
-; Imports the clojure.string namespace as 'str' alias
 (require '[clojure.string :as str])
 
-; hardcodes the file path to the CSV
+; path to file
 (def filepath "./data/city_temperatures.csv")
 
-; parse into rows using slup and str/split by new lines #"\n"
-(def rows (str/split (slurp filepath) #"\n"))
-
-; discard header using the `rest` function
-; rows is a list like this ("Los Angeles,70.5" "Honolulu,80.3" ...)
-(def rows (rest rows))
-
-; map from a list of rows to:
-; 				{Los Angeles (70.5), Honolulu (80.3), South Bend (55.8), Los Angeles (71.2) ...}
-
-;;; first declare callback funtion `parse-row` that maps each row to {"city" (temperature)}
-; (defn parse-row [row] (let
-; 																								[column (str/split row #",")
-; 																									city (first column)
-; 																									temperature (Float/parseFloat (nth column 1))]
-; 																									{city (list temperature)}
-; 																						)
-; )
+; read full file contents and assign to rows
+(def rows (rest (str/split-lines (slurp filepath))))
+(println rows)
+; ; parse each row
+(defn parse-row [row]
+  (let [[city temp] (str/split row #",")]
+    [city (Float/parseFloat temp)]))
 
 
-; ;;; apply the callback function to each element in the `rows` list 
-; (def rows (map parse-row rows))
+; ; create a map like this {"city": [t1, t2,..,]}
+(def city-temps
+  (group-by first (map parse-row rows)))
 
-; ; use the `reduce` function to merge temperatures by city to create a map like this:
-; ; {"Los Angeles" (70.5 71.2 ...), "Honolulu" (80.3 81.0 ...), "South Bend" (55.8 54.5 ...)}
-; ; store it on a variable `temperatures_map`
-; ;;;; map-merger: function that merge two maps
-; (defn map-merger [x y] (concat (or x '()) y))
-; ; x = e1[e2_city] --> nil
-; ; y = e2_temperature
-; ;;;; city-reducer: callback that takes `e1` `e2` and updates `e1` with what is in `e2`
-; (defn city-reducer [e1 e2]
-; 																		(let [e2_city (key (first e2))
-; 																								e2_temperature (val (first e2))] 
-; 																								(update e1 e2_city map-merger e2_temperature)
-; 																								)
-; )
+; ; a function that computes averages
+(defn avg [temps] (/ (reduce + temps) (count temps)))
 
 
-; (def temperatures_map (reduce city-reducer rows))
-; (println temperatures_map)
-
-; ; ; Average function
-; (defn avg [p] (double (/ (reduce + p)  (count p))))
-; ; Use `doseq` to compute the statistics per city
-; (println "Statistics temperatures per city")
-; ; for city, temperatures in temperatures_map.items():
-; (doseq [[city temperatures] temperatures_map]
-; 					 (println city 
-; 					 			"\n\tmin\t" (apply min temperatures)
-; 					 			"\n\tmax\t" (apply max temperatures)
-; 					 			"\n\tavg\t" (avg temperatures)))
-
+; doseq is used for iteration with side effects (like printing), not for producing a value.
+; Loop through a collection and destructure it
+(doseq [[city entries] city-temps]
+  (let [temps (map second entries)]
+    (println city)
+    (println "\tmin:" (apply min temps))
+    (println "\tmax:" (apply max temps))
+    (println "\tavg:" (avg temps))))
